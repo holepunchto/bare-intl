8f2a58a1c6ac29bdb748358580bd8e539e5d1930
Yasser Nascimento <dev@yasser.email>
2025-09-01 17:54:46 -0300

    Use `Intl.js` implementation

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-exports.Collator = require('./lib/collator')
-exports.DateTimeFormat = require('./lib/date-time-format')
-exports.DisplayNames = require('./lib/display-names')
-exports.DurationFormat = require('./lib/duration-format')
-exports.ListFormat = require('./lib/list-format')
-exports.Locale = require('./lib/locale')
-exports.NumberFormat = require('./lib/number-format')
-exports.PluralRules = require('./lib/plural-rules')
-exports.RelativeTimeFormat = require('./lib/relative-time-format')
-exports.Segmenter = require('./lib/segmenter')
-
-// https://tc39.es/ecma402/#sec-intl.getcanonicallocales
-exports.getCanonicalLocales = function getCanonicalLocales() {
-  throw new Error('Not implemented')
-}
+module.exports = require('intl')
